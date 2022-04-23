6e5fe62042727c744ee772c593dfe90e11368c0d
ichsanna <43778566+ichsanna@users.noreply.github.com>
2022-04-23 12:50:33 +0700

    Add Morgan

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const morgan = require('morgan')
 
 const userRoutes = require('./routes/userRouter')
 const fileRoutes = require('./routes/fileRouter')
@@ -10,6 +11,7 @@ const msg = require('./configs/responseMessages')
 
 const app = express()
 
+app.use(morgan('tiny'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
