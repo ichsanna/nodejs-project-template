c9cfb2e93afbdf8ddb65c96b3b6bcbd7b4672074
ichsanna <43778566+ichsanna@users.noreply.github.com>
2022-04-24 17:06:39 +0700

    Refactor: rewrite controller functions

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -6,32 +6,41 @@ const resFormat = require('../configs/responseFormat')
 const fileMethods = {
     uploadFile: (req, res) => {
         try {
-            let filename = req.file.originalname
-            let filetype = req.file.mimetype
-            let filesize = req.file.size
-            res.status(201).json(resFormat(true,msg.successUpload,{
-                filename: filename,
-                filetype: filetype,
-                filesize: filesize
-            }))
+            let data = async () => {
+                let { filename, filetype, filesize } = req.file
+                return {
+                    status: 201, data: resFormat(true, msg.successUpload, {
+                        filename: filename,
+                        filetype: filetype,
+                        filesize: filesize
+                    })
+                }
+            }
+            let resp = await data()
+            res.status(resp.status).json(resp.data)
         }
         catch (err) {
-            res.status(400).json(resFormat(false,null,err))
+            res.status(400).json(resFormat(false, null, err))
         }
     },
     downloadFile: (req, res) => {
         try {
-            let filename = req.params.filename
-            const location = `${__dirname}/../uploads/`+filename;
-            if (fs.existsSync(location)){
-                res.status(200).download(location);
+            let data = async () => {
+                let filename = req.params.filename
+                const location = `${__dirname}/../uploads/` + filename;
+                if (fs.existsSync(location)) {
+                    return { status: 200, data: location }
+                }
+                else {
+                    return { status: 404, data: resFormat(false, msg.noFileFound, null) }
+                }
             }
-            else{
-                res.status(404).json(resFormat(false,msg.noFileFound,null))
-            }
+            let resp = await data()
+            if (resp.status === 200 ) res.status(resp.status).download(resp.data);
+            else res.status(resp.status).json(resp.data)
         }
         catch (err) {
-            res.status(400).json(resFormat(false,null,err))
+            res.status(400).json(resFormat(false, null, err))
         }
     }
 }
--- controllers/userController.js
+++ controllers/userController.js
@@ -8,78 +8,81 @@ const resFormat = require('../configs/responseFormat')
 const userMethods = {
     getUserById: async (req, res) => {
         try {
-            let id = req.params.id
-            let data = await User.findById(id).select('username created')
-            if (data){
-                res.status(200).json(resFormat(true,msg.successGetUser,data))
-            }
-            else{
-                res.status(404).json(resFormat(false,msg.noUser,data))
+            let data = async () => {
+                let id = req.params.id
+                let data = await User.findById(id).select('username created')
+                if (data) {
+                    return { status: 200, data: resFormat(true, msg.successGetUser, data) }
+                }
+                return { status: 404, data: resFormat(false, msg.noUser, data) }
             }
         }
         catch (err) {
-            res.status(400).json(resFormat(false,null,err))
+            res.status(400).json(resFormat(false, null, err))
         }
     },
     getAllUsers: async (req, res) => {
         try {
-            let users = await User.find().select('username created').exec()
-            if (users) {
-                res.status(200).json(resFormat(true,msg.successGetUsers,users))
+            let data = async () => {
+                let users = await User.find().select('username created').exec()
+                if (users) {
+                    return { status: 200, data: resFormat(true, msg.successGetUsers, data) }
+                }
+                return { status: 404, data: resFormat(false, msg.noUser, data) }
             }
-            else{
-                res.status(404).json(resFormat(false,msg.noUser,data))
-            }
+            let resp = await data()
+            res.status(resp.status).json(resp.data)
         }
         catch (err) {
-            res.status(400).json(resFormat(false,null,err))
+            res.status(400).json(resFormat(false, null, err))
         }
     },
-    userLogin: async (req,res)=>{
+    userLogin: async (req, res) => {
         try {
-            let username = req.body.username
-            let password = req.body.password
-            let getUser = await User.findOne({ username: username })
-            if (getUser) {
-                let comparePassword = await bcrypt.compare(password, getUser.password)
-                let token = signToken(username)
-                if (comparePassword) {
-                    res.status(200).json(resFormat(true,msg.successLogin,{ token: token }))
+            let data = async () => {
+                let { username, password } = req.body
+                let getUser = await User.findOne({ username: username })
+                if (getUser) {
+                    let comparePassword = await bcrypt.compare(password, getUser.password)
+                    let token = signToken(username)
+                    if (comparePassword) {
+                        return { status: 200, data: resFormat(true, msg.successLogin, { token: token }) }
+                    }
+                    return { status: 401, data: resFormat(false, msg.incorrectUsernamePassword, null) }
+                }
+                return { status: 404, data: resFormat(false, msg.incorrectUsernamePassword, null) }
+            }
+            let resp = await data()
+            res.status(resp.status).json(resp.data)
+        }
+        catch (err) {
+            res.status(400).json(resFormat(false, null, err))
+        }
+    },
+    userRegister: async (req, res) => {
+        try {
+            let data = async () => {
+                let { username, password } = req.body
+                let getUser = await User.findOne({ username: username }).select('username created')
+                if (getUser) {
+                    return { status: 409, data: resFormat(false, msg.duplicateUsername, null) }
                 }
                 else {
-                    res.status(401).json(resFormat(false,msg.incorrectUsernamePassword,null))
+                    let encryptedPassword = await bcrypt.hash(password, 10)
+                    let newUser = new User({
+                        username: username,
+                        password: encryptedPassword
+                    })
+                    newUser.save()
+                    let token = signToken(username)
+                    return { status: 201, data: resFormat(true, msg.successLogin, { token: token }) }
                 }
             }
-            else {
-                res.status(404).json(resFormat(false,msg.incorrectUsernamePassword,null))
-            }
+            let resp = await data()
+            res.status(resp.status).json(resp.data)
         }
         catch (err) {
-            res.status(400).json(resFormat(false,null,err))
-        }
-    },
-    userRegister: async (req,res)=>{
-        try {
-            let username = req.body.username
-            let password = req.body.password
-            let getUser = await User.findOne({ username: username }).select('username created')
-            if (getUser) {
-                res.status(409).json(resFormat(false,msg.duplicateUsername,getUser))
-            }
-            else {
-                let encryptedPassword = await bcrypt.hash(password, 10)
-                let newUser = new User({
-                    username: username,
-                    password: encryptedPassword
-                })
-                
-                newUser.save()
-                let token = signToken(username)
-                res.status(201).json(resFormat(true,msg.successLogin,{ token: token }))
-            }
-        }
-        catch (err) {
-            res.status(400).json(resFormat(false,null,err))
+            res.status(400).json(resFormat(false, null, err))
         }
     }
 }
